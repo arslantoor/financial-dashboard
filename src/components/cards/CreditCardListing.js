326458e1985f3e5acba4ba3dd919caf39d186259
arslantoor <muhammadarslantoor@gmail.com>
2025-02-28 18:46:02 +0500

    Added: Tooltip using react portal

--- src/components/cards/CreditCardListing.js
+++ src/components/cards/CreditCardListing.js
@@ -1,6 +1,7 @@
+import Tooltip from '@/components/common/tooltip/Tooltip.js'
 import React from 'react'
-import CardItem from './CardItem'
 import { useNavigate } from 'react-router-dom'
+import CardItem from './CardItem'
 
 const CreditCards = ({ cards }) => {
   const navigate = useNavigate()
@@ -8,12 +9,14 @@ const CreditCards = ({ cards }) => {
     <div className="md:col-span-1 lg:col-span-2">
       <div className="flex justify-between items-center mb-2 sm:mb-3">
         <h2 className="text-lg sm:text-xl md:text-2xl font-semibold text-gray-800">My Cards</h2>
-        <button
-          onClick={() => navigate('/credit-cards')}
-          className="text-base text-blue-600 hover:text-blue-700 transition"
-        >
-          See All
-        </button>
+        <Tooltip text="View all cards" position="bottom">
+          <button
+            onClick={() => navigate('/credit-cards')}
+            className="text-base text-blue-600 hover:text-blue-700 transition"
+          >
+            See All
+          </button>
+        </Tooltip>
       </div>
       <div className="flex overflow-x-auto gap-3 sm:gap-4 md:gap-6">
         {cards.slice(0, 2).map((card) => (
